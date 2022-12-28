2aff0268595a74789b50467aea3391afee33004c
Nahom_github <93624579+Nahom-github@users.noreply.github.com>
2022-12-28 18:29:14 -0500

    MongoDB integration
    we can access NBA data from the online server

--- server.js
+++ server.js
@@ -13,3 +13,28 @@ app.get('/', (req, res) => {
 app.listen(3000);
 console.log("Listening on port 3000, Link: http://localhost:3000");
 
+
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your connection string.
+const uri =
+  "mongodb+srv://adminUser:123@cluster0.tililof.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
