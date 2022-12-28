5ea90d604f8fb253129cb89546698711a50bd2ef
Jsamuda25 <jsamuda25@gmail.com>
2022-12-28 16:13:30 -0500

    Modified home header
    In the process of adding hamburger bar.

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 
 app.set("view engine", "pug");
-
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 app.get('/', (req, res) => {
-  res.render('home');
+   res.render('home');
 });
+
 //test123
 app.listen(3000);
 console.log("Listening on port 3000, Link: http://localhost:3000");
