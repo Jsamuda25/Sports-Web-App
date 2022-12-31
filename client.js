84c87fc597cca2c87d06ffe80af5ce872ae9a4a0
Jsamuda25 <jsamuda25@gmail.com>
2022-12-31 12:58:42 -0500

    Adding buddy hield season to monogdb

--- client.js
+++ client.js
@@ -1,3 +1,38 @@
+// const data = null;
+// var XMLHttpRequest = require('xhr2');
+// const xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+
+// xhr.addEventListener("readystatechange", function () {
+// 	if (this.readyState === this.DONE) {
+// 		console.log(this.responseText);
+// 	}
+// });
+
+// xhr.open("GET", "https://api-nba-v1.p.rapidapi.com/players/statistics?id=236&season=2020");
+// xhr.setRequestHeader("X-RapidAPI-Key", "b356bda966msh2b90a52aa160c7bp1d273bjsnbc426c602ac7");
+// xhr.setRequestHeader("X-RapidAPI-Host", "api-nba-v1.p.rapidapi.com");
+
+// xhr.send(data);
+
+// const data = null;
+// var XMLHttpRequest = require('xhr2');
+// const xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+
+// xhr.addEventListener("readystatechange", function () {
+// 	if (this.readyState === this.DONE) {
+// 		console.log(this.responseText);
+// 	}
+// });
+
+// xhr.open("GET", "https://free-nba.p.rapidapi.com/players/237");
+// xhr.setRequestHeader("X-RapidAPI-Key", "b356bda966msh2b90a52aa160c7bp1d273bjsnbc426c602ac7");
+// xhr.setRequestHeader("X-RapidAPI-Host", "free-nba.p.rapidapi.com");
+
+// xhr.send(data);
+
+
 const data = null;
 var XMLHttpRequest = require('xhr2');
 const xhr = new XMLHttpRequest();
@@ -9,8 +44,8 @@ xhr.addEventListener("readystatechange", function () {
 	}
 });
 
-xhr.open("GET", "https://api-nba-v1.p.rapidapi.com/players/statistics?id=236&season=2020");
+xhr.open("GET", "https://free-nba.p.rapidapi.com/stats?per_page=25&page=0");
 xhr.setRequestHeader("X-RapidAPI-Key", "b356bda966msh2b90a52aa160c7bp1d273bjsnbc426c602ac7");
-xhr.setRequestHeader("X-RapidAPI-Host", "api-nba-v1.p.rapidapi.com");
+xhr.setRequestHeader("X-RapidAPI-Host", "free-nba.p.rapidapi.com");
 
 xhr.send(data);
