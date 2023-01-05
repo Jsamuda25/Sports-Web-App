785b83cbe19261979114cd0aca1549243c2cfc0f
Jsamuda25 <jsamuda25@gmail.com>
2023-01-04 21:07:04 -0500

    Searching for and loading team data
    - Using JSON file(s) to insert team data per season (in rapidAPI it is statistics-> team statistics)
    
    - Added basic front end to show team stats on page

--- api-parser.js
+++ api-parser.js
@@ -1,3 +1,4 @@
+const { Template } = require("ejs");
 const fs = require("fs")
 let mongo = require('mongodb');
 let MongoClient = mongo.MongoClient;
@@ -141,43 +142,236 @@ for (game of player_api_data.response) {
 console.log(players);
 
 // parsing team stats
-let team_input = JSON.parse(fs.readFileSync("celtics2019.json"));
-console.log(team_input);
-console.log(team_input.parameters.id);
+let team_input = JSON.parse(fs.readFileSync("bucks2019.json"));
+// console.log(team_input);
+// console.log(team_input.parameters.id);
 
-let team = {};
+let team={};
+
 
 //identify team name based on team id in the Rapid API database
 // for example, an end point with team id 2 is the Boston Celtics
 if(team_input.parameters.id == 1){
-  team[team_input.parameters.id].teamId = 1;
-  team[team_input.parameters.id].teamName = "Atlanta Hawks"; 
+  team.teamId = 1;
+  team.teamName = "Atlanta Hawks"; 
 }
 
 else if(team_input.parameters.id == 2){
-  team[team_input.parameters.id].teamId = 2;
-  team[team_input.parameters.id].teamName = "Boston Celtics"; 
+  team.teamId = 2;
+  team.teamName = "Boston Celtics"; 
 }
 
 else if(team_input.parameters.id == 4){
-  team[team_input.parameters.id].teamId = 4;
-  team[team_input.parameters.id].teamName = "Brooklyn Nets";
+  team.teamId = 4;
+  team.teamName = "Brooklyn Nets";
 }
 
 else if(team_input.parameters.id == 5){
-  team[team_input.parameters.id].teamId = 5;
-  team[team_input.parameters.id].teamName = "Charlotte Hornets";
+  team.teamId = 5;
+  teamteamName = "Charlotte Hornets";
 }
 
 else if(team_input.parameters.id == 6){
-  team[team_input.parameters.id].teamId = 6;
-  team[team_input.parameters.id].teamName = "Chicago Bulls";
+  team.teamId = 6;
+  team.teamName = "Chicago Bulls";
 }
 
 else if(team_input.parameters.id == 7){
-  team_input[team_input.parameters.id].teamId = 7;
-  team_input[team_input.parameters.id].teamName = "Cleveland Cavaliers";
+  team_input.teamId = 7;
+  team_input.teamName = "Cleveland Cavaliers";
 }
+
+else if(team_input.parameters.id == 10){
+  team.teamId = 10;
+  team.teamName = "Detroit Pistons";
+}
+
+else if(team_input.parameters.id == 15){
+  team.teamId = 15;
+  team.teamName = "Indiana Pacers";
+}
+
+else if(team_input.parameters.id == 20){
+  team.teamId = 20;
+  team.teamName = "Miami Heat";
+}
+
+else if(team_input.parameters.id == 21){
+  team.teamId = 21;
+  team.teamName = "Milwaukee Bucks";
+}
+
+else if(team_input.parameters.id == 24){
+  team.teamId = 24;
+  team.teamName = "New York Knicks";
+}
+
+else if(team_input.parameters.id == 26){
+  team.teamId = 26;
+  team.teamName = "Orlando Magic";
+}
+
+else if(team_input.parameters.id == 27){
+  team.teamId = 27;
+  team.teamName = "Philadelphia 76ers";
+}
+
+else if(team_input.parameters.id == 38){
+  team.teamId = 38;
+  team.teamName = "Toronto Raptors";
+}
+
+else if(team_input.parameters.id == 41){
+  team.teamId = 41;
+  team.teamName = "Washington Wizards";
+}
+
+else if(team_input.parameters.id == 8){
+  team.teamId = 8;
+  team.teamName = "Dallas Mavericks";
+}
+
+else if(team_input.parameters.id == 9){
+  team.teamId = 9;
+  team.teamName = "Denver Nuggets";
+}
+
+else if(team_input.parameters.id == 11){
+  team.teamId = 11;
+  team.teamName = "Golden State Warriors";
+}
+
+else if(team_input.parameters.id == 14){
+  team.teamId = 14;
+  team.teamName = "Houston Rockets";
+}
+
+else if(team_input.parameters.id == 16){
+  team.teamId = 16;
+  team.teamName = "LA Clippers";
+}
+
+else if(team_input.parameters.id == 17){
+  team.teamId = 17;
+  team.teamName = "LA Lakers";
+}
+
+else if(team_input.parameters.id == 19){
+  team.teamId = 19;
+  team.teamName = "Memphis Grizzlies";
+}
+
+else if(team_input.parameters.id == 22){
+  team.teamId = 22;
+  team.teamName = "Minnesota Timberwolves";
+}
+
+else if(team_input.parameters.id == 23){
+  team.teamId = 23;
+  team.teamName = "New Orleans Pelicans";
+}
+
+else if(team_input.parameters.id == 25){
+  team.teamId = 25;
+  team.teamName = "Oklahoma City Thunder";
+}
+else if(team_input.parameters.id == 28){
+  team.teamId = 28;
+  team.teamName = "Phoenix Suns";
+}
+
+else if(team_input.parameters.id == 29){
+  team.teamId = 29;
+  team.teamName = "Portland Trail Blazers";
+}
+
+else if(team_input.parameters.id == 30){
+  team.teamId = 30;
+  team.teamName = "Sacramento Kings";
+}
+
+else if(team_input.parameters.id == 31){
+  team.teamId = 31;
+  teamteamName = "San Antonio Spurs";
+}
+
+else if(team_input.parameters.id == 102){
+  team.teamId = 102;
+  team.teamName = "Utah Jazz";
+}
+
+else{
+  
+}
+
+
+
+
+// parsing team stats
+
+//team.totals = team_input.games;
+console.log(team_input.parameters.id);
+// console.log(team_input.response.games);
+team.season = team_input.parameters.season;
+let teamTotals = {
+  totalGames: team_input.response.games,
+  fastBreakPoints:team_input.response.fastBreakPoints,
+  pointsInPaint: team_input.response.pointsInPaint,
+  secondChancePoints: team_input.response.secondChancePoints,
+  pointsOffTurnovers: team_input.response.pointsOffTurnovers,
+  biggestLead: team_input.response.biggestLead,
+  points: team_input.response.points,
+  fgm: team_input.response.fgm,
+  fga: team_input.response.fga,
+  fgp: team_input.response.fgp,
+  fta: team_input.response.fta,
+  ftp: team_input.response.ftp,
+  ftm: team_input.response.ftm,
+  tpm: team_input.response.tpm,
+  tpa: team_input.response.tpa,
+  tpp: team_input.response.tpp,
+  offReb: team_input.response.offReb,
+  defReb: team_input.response.defReb,
+  totReb: team_input.response.totReb,
+  assists: team_input.response.assists,
+  pFouls: team_input.response.pFouls,
+  steals: team_input.response.steals,
+  turnovers: team_input.response.turnovers,
+  blocks: team_input.response.blocks,
+  plusMinus: team_input.response.plusMinus,
+}
+
+let teamAverages = {
+  avgGames: (team_input.response.games)/ (team_input.response.games).toFixed(2),
+  avgFastBreakPoints: (team_input.response.fastBreakPoints)/ (team_input.response.games),
+  avgPointsInPaint: (team_input.response.pointsInPaint)/ (team_input.response.games),
+  avgSecondChancePoints: (team_input.response.secondChancePoints)/ (team_input.response.games),
+  avgPointsOffTurnovers: (team_input.response.pointsOffTurnovers)/ (team_input.response.games),
+  avgPoints: (team_input.response.points)/ (team_input.response.games),
+  avgFgm: (team_input.response.fgm)/ (team_input.response.games),
+  avgFga: (team_input.response.fga)/ (team_input.response.games),
+  avgFgp: (team_input.response.fgp)/ (team_input.response.games),
+  avgFta: (team_input.response.fta)/ (team_input.response.games),
+  avgFtm: (team_input.response.ftm)/ (team_input.response.games),
+  avgFtp: (team_input.response.ftp)/ (team_input.response.games),
+  avgTpm: (team_input.response.tpm)/ (team_input.response.games),
+  avgTpa: (team_input.response.tpa)/ (team_input.response.games),
+  avgTpp: (team_input.response.tpp)/ (team_input.response.games),
+  avgOffReb: (team_input.response.offReb)/ (team_input.response.games),
+  avgDefReb: (team_input.response.defReb)/ (team_input.response.games),
+  avgTotReb: (team_input.response.totReb)/ (team_input.response.games),
+  avgAssists: (team_input.response.assists)/ (team_input.response.games),
+  avgPFouls: (team_input.response.pFouls)/ (team_input.response.games),
+  avgSteals: (team_input.response.steals)/ (team_input.response.games),
+  avgTurnovers: (team_input.response.turnovers)/ (team_input.response.games),
+  avgBlocks: (team_input.response.blocks)/ (team_input.response.games),
+  avgPlusMinus: (team_input.response.plusMinus)/ (team_input.response.games),
+}
+
+team.statTotals= teamTotals;
+team.perGameAverages = teamAverages;
+console.log(team)
+
 
 MongoClient.connect("mongodb+srv://adminUser:123@cluster0.tililof.mongodb.net/test", { useNewUrlParser: true }, function(err, client) {
   if (err) throw err;
@@ -186,13 +380,18 @@ MongoClient.connect("mongodb+srv://adminUser:123@cluster0.tililof.mongodb.net/te
   // db.dropDatabase("player_data", function(err, delOK) {
   //   // if (err) throw err;
   //   if (delOK) console.log("Collection deleted");
-
+  // });
     // Insert player_data
     db.collection("player_data").insertMany(Object.values(players), function(err, res) {
       if (err) throw err;
       console.log("Number of documents inserted: " + res.insertedCount);
       process.exit();
     });
-  // });
+
+    // Insert team_data
+    db.collection("team_data").insertOne(team, function(err, res) {
+      if (err) throw err;
+    });
+
 
 });
--- routes/team-stats.js
+++ routes/team-stats.js
@@ -1,82 +1,72 @@
 const express = require('express');
-
+let ObjectId = require('mongodb').ObjectId;
 let router = express.Router()
 router.use(express.json());
 router.use(express.urlencoded({extended: true}));
 
 router.get('/', (req, res) => {
-    res.render('team-stats');
+    res.render('team-lookup');
 });
 
-router.get("/player", getPlayer);
+router.get("/teams", getTeams);
+
+router.get("/team/:id", getTeam);
 
 // Get player from database based on query with firstname, lastname, team, and year 
-async function getPlayer(req, res) {
-    // try{
-    //     const searchResult = await db.collection("users").findOne({username: req.body.username, password:req.body.password});
-    //     if(searchResult == null){ // no such user exists
-    //         res.status(200).render("./pages/error",{message: "Account does not exist!"});
-    //         //res.send("Account does not exist!");
-    //     }
-    //     else{
-    //         // update session attributes to match the attributes of user found in database
+async function getTeams(req, res) {
 
-    //         res.redirect('/user/home')
-    //         res.status(200).send();
-          
-    //     }
-    // }
-    // catch(err){
-    //     res.status(500).json({ error: "Error logging in."});
-    // }
+	let query = {};
 
-    let query = {};
-
-	if(req.query.team){
-        query.team = {name: req.query.team};
+	if(req.query.teamName){
+		query.teamName = {"$regex":req.query.teamName,"$options":"xi"}
+	}
+		
+	if(req.query.season){
+		query.season = req.query.season;
 	}
 
-	if(req.query.year){
-		query.year = req.query.year;
-	}
+	console.log(query);
 
-	if(req.query.firstname){
-		query.firstname = {"$regex":req.query.firstname,"$options":"i"}
-	}
-
-	if(req.query.lastname){
-		query.lastname = {"$regex":req.query.lastname,"$options":"i"};
-	}
-
-	req.local.db.collection("player_data").find(query).toArray(function(err, searchResult){
+	req.app.locals.db.collection("team_data").find(query).project({_id: 1, teamName: 1, season: 1}).toArray(function(err, searchResult){
 		if(err){
 			console.log("Server Error");
-            res.sendStatus(503);
+            res.sendStatus(500);
+            return;
 		}
 
-		if(!searchResult.length){
+		if(!searchResult){
 			console.log("No Match!")
+			res.status(404)
+			res.send("No match!");
+			return;
+		}
+
+		else{
+			res.status(200).render("team-results", {teams: searchResult});
 			console.log(searchResult);
+		}
+	});
+}
+
+function getTeam(req,res){
+	let id = req.params.id;
+	req.app.locals.db.collection("team_data").findOne({_id: new ObjectId(id)}, function(err, team){
+		if(err){
+			console.log("Server Error");
+			res.sendStatus(500);
+			return;
+		}
+
+		if(!team){
+			console.log("No Match!")
 			res.status(404)
 			res.send("No match!");
 			return;
 		}
 		else{
-			console.log(searchResult);
-			res.status(200);
-			res.send(searchResult);
+			res.status(200).render("team", {team: team});
+			console.log(team);
 		}
-
 	});
-
-
-
-
 }
-
-
-
-
-
-
 module.exports = router;
